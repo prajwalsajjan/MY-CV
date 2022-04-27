038dfd08eb8eb9c58dbc261f9ab3e45e5f2ccd2a
prajwalsajjan <104249189+prajwalsajjan@users.noreply.github.com>
2022-04-27 10:45:35 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+$yourname = $_POST['yourname'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+// database creation
+$conn = new mysqli('localhost','root','','form inputs');
+if($_conn->connect_error);{
+die('Connection Failed :' .$conn-.connect_error);
+}else{
+    $stmt = $conn->prepare("insert into registration (yourname, email, message ) values(?,?,?) ");
+    $stmt->bind_param("sss", $yourname,$email,$message);
+    $stmt->execute();
+    echo "message sent successfully..." ;
+    $stmt->close();
+    $conn->close();
+}
+
+
+?>
